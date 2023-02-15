c39faac1f6760b8bf226123b4ff6d5a833efb494
Franco Bonilla (he/him) <bonilla.franco484@gmail.com>
2023-02-15 02:32:54 -0700

    Created Android Studio Project w/ basic pages

---
+++ app/src/main/java/com/example/qrky/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.qrky;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
