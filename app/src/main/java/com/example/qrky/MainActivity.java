86740b72400eb176add9f05056e1ceaafb8e4d7b
Franco Bonilla (he/him) <bonilla.franco484@gmail.com>
2023-02-18 17:51:52 -0700

    attempt 2 at pushing the nav bar

--- app/src/main/java/com/example/qrky/MainActivity.java
+++ app/src/main/java/com/example/qrky/MainActivity.java
@@ -2,15 +2,8 @@ package com.example.qrky;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,13 +12,5 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Initialize the bottom navigation view
-        //create bottom navigation view object
-        BottomNavigationView bttmNavView = (BottomNavigationView) findViewById(R.id.bottom_navigating_view);
-        NavHostFragment bttmNavHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_fragment);
-        NavController bttmNavController = bttmNavHostFragment.getNavController();
-        NavigationUI.setupWithNavController(bttmNavView, bttmNavController);
-
     }
 }
