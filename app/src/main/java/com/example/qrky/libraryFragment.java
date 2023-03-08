abdd7824bbd559eafc9313f20c6e9202f6d9fa2f
AARON <abinoy@ualberta.ca>
2023-03-08 14:57:22 -0700

    the base library

--- app/src/main/java/com/example/qrky/libraryFragment.java
+++ app/src/main/java/com/example/qrky/libraryFragment.java
@@ -1,64 +1,94 @@
 package com.example.qrky;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link libraryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class libraryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerView;
+    private CardAdapter adapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<CardData> allCards;
+    private List<CardData> filteredCards;
+    private SearchView searchView;
 
     public libraryFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment libraryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static libraryFragment newInstance(String param1, String param2) {
-        libraryFragment fragment = new libraryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//...
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false);
+        View view = inflater.inflate(R.layout.fragment_library, container, false);
+
+        recyclerView = view.findViewById(R.id.normal_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
+        allCards = cards();
+        filteredCards = new ArrayList<>(allCards);
+
+        adapter = new CardAdapter(filteredCards);
+        recyclerView.setAdapter(adapter);
+
+        searchView = view.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filteredCards.clear();
+                if (newText.isEmpty()) {
+                    filteredCards.addAll(allCards);
+                } else {
+                    for (CardData card : allCards) {
+                        if (card.getTitle().toLowerCase().contains(newText.toLowerCase())) {
+                            filteredCards.add(card);
+                        }
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        return view;
     }
+
+
+    private List<CardData> cards() {
+        List<CardData> cards = new ArrayList<>();
+        String[] titles = {"Items 1", "Item 2", "Items 3", "Item 4"};
+
+        for (int i = 0; i < titles.length; i++) {
+            String title = titles[i];
+            int score = 0;
+            for (int j = 0; j < title.length(); j++) {
+                char c = title.charAt(j);
+                if (Character.isLetter(c)) {
+                    score += (int) c;
+                }
+            }
+            cards.add(new CardData(title, score));
+        }
+
+        return cards;
+    }
+
 }
+
