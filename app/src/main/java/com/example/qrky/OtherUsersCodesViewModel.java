50d24eafceacc2eee95006e659e1378c40051860
Franco <bonilla.franco484@gmail.com>
2023-03-07 17:39:24 -0700

    Other users library xml + viewmodel + java

---
+++ app/src/main/java/com/example/qrky/OtherUsersCodesViewModel.java
@@ -0,0 +1,8 @@
+package com.example.qrky;
+
+import androidx.lifecycle.ViewModel;
+
+public class OtherUsersCodesViewModel extends ViewModel {
+    // US 02.03.01: As a player, I want to be able to browse QR codes that other players have scanned.
+    // Franco Bonilla
+}
