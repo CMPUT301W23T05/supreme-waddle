804900c8897c0efaf67bfd4707b0d621756cfd8a
AARON <abinoy@ualberta.ca>
2023-03-09 14:43:32 -0700

    added marker to show nearby codes on map and created a nearby codes page

--- app/src/main/java/com/example/qrky/MapsFragment.java
+++ app/src/main/java/com/example/qrky/MapsFragment.java
@@ -1,5 +1,7 @@
 package com.example.qrky;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -9,9 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.LocationServices;
@@ -25,20 +29,36 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 import android.Manifest;
 import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.content.DialogInterface;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
-
+import java.util.List;
 
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+
+
+
 
     private static final int PERMISSION_REQUEST_CODE = 1;
     private static final int MY_PERMISSIONS_REQUEST_LOCATION = 123;
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-
+    private Button nearbyCodesButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,8 +82,19 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
         }
 
+        // Get the button view and set the click listener
+        nearbyCodesButton = rootView.findViewById(R.id.nearby_codes_button);
+        nearbyCodesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_fragment);
+                navController.navigate(R.id.nearbyCodesFragment);
+            }
+        });
+
         return rootView;
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -95,6 +126,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
         }
+
+        loadQRCodesFromFirebase();
     }
 
     @Override
@@ -112,4 +145,38 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
             }
         }
     }
+
+    private void loadQRCodesFromFirebase() {
+        // Access the Firestore instance
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Access the "QR Codes" collection
+        CollectionReference qrCodesRef = db.collection("QR Codes");
+
+        // Build a query to retrieve documents that have a "Location" attribute with a non-null geopoint value
+        Query query = qrCodesRef.whereNotEqualTo("Location", null);
+
+
+        // Execute the query
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                // Retrieve the documents from the query result
+                QuerySnapshot querySnapshot = task.getResult();
+                List<DocumentSnapshot> documents = querySnapshot.getDocuments();
+
+                // Do something with the documents
+                for (DocumentSnapshot document : documents) {
+                    Log.d(TAG, "Document data: " + document.getData());
+                    String qrCodeData = document.getString("data");
+                    GeoPoint qrCodeLocation = document.getGeoPoint("Location");
+                    LatLng latLng = new LatLng(qrCodeLocation.getLatitude(), qrCodeLocation.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(qrCodeData));
+                }
+            } else {
+                Log.w(TAG, "Error getting documents.", task.getException());
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/qrky/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/qrky/MyItemRecyclerViewAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.qrky.placeholder.PlaceholderContent.PlaceholderItem;
-import com.example.qrky.databinding.FragmentNearbyCodesBinding;
 
 import java.util.List;
 
@@ -16,19 +15,19 @@ import java.util.List;
  * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
  * TODO: Replace the implementation with code for your data type.
  */
-public class MyNearCodeRecyclerViewAdapter extends RecyclerView.Adapter<MyNearCodeRecyclerViewAdapter.ViewHolder> {
+public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecyclerViewAdapter.ViewHolder> {
 
     private final List<PlaceholderItem> mValues;
 
-    public MyNearCodeRecyclerViewAdapter(List<PlaceholderItem> items) {
+    public MyItemRecyclerViewAdapter(List<PlaceholderItem> items) {
         mValues = items;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        return new ViewHolder(FragmentNearbyCodesBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
-
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_nearby_codes_list, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -36,6 +35,13 @@ public class MyNearCodeRecyclerViewAdapter extends RecyclerView.Adapter<MyNearCo
         holder.mItem = mValues.get(position);
         holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Implement item click listener
+            }
+        });
     }
 
     @Override
@@ -44,14 +50,16 @@ public class MyNearCodeRecyclerViewAdapter extends RecyclerView.Adapter<MyNearCo
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
         public final TextView mIdView;
         public final TextView mContentView;
         public PlaceholderItem mItem;
 
-        public ViewHolder(FragmentNearbyCodesBinding binding) {
-            super(binding.getRoot());
-            mIdView = binding.itemNumber;
-            mContentView = binding.content;
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mContentView = (TextView) view.findViewById(R.id.content);
         }
 
         @Override
@@ -59,4 +67,5 @@ public class MyNearCodeRecyclerViewAdapter extends RecyclerView.Adapter<MyNearCo
             return super.toString() + " '" + mContentView.getText() + "'";
         }
     }
+
 }
