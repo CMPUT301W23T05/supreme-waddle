04f60f577adcfd4ce402d13b5a88cf5d8c1d900e
Franco <bonilla.franco484@gmail.com>
2023-03-09 19:33:55 -0700

    OtherUsers' Library made; not set up with db

--- app/src/main/java/com/example/qrky/OtherUsersCodes.java
+++ app/src/main/java/com/example/qrky/OtherUsersCodes.java
@@ -1,37 +1,86 @@
 package com.example.qrky;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
+// US 02.03.01: As a player, I want to be able to browse QR codes that other players have scanned.
+// Artist: Franco Bonilla
+// Actors: Player (primary)
+// Goal: Player sees the code library of other players
+// Trigger: Player clicks on an OtherUser
+// Precondition: N/A
+// Postcondition: Player sees OtherUser's code library
+// Basic Flow:
+//     1. Player clicks on an OtherUser (trigger)
+//     2. System gets code library from database
+//     3. System makes card for each code in library
+//     4. System displays cards in a grid (3 columns)
+//     5. Player sees OtherUser's code library (goal)
 
 public class OtherUsersCodes extends Fragment {
-    // US 02.03.01: As a player, I want to be able to browse QR codes that other players have scanned.
-    // Franco Bonilla
-    private OtherUsersCodesViewModel mViewModel;
 
+//    FragmentOtherUsersCodesBinding binding;
+    private OtherUsersCodesViewModel mViewModel;
+    private GridView gridOfCodes;
+    private ImageButton backBttn;
+
+    public OtherUsersCodes() {}
     public static OtherUsersCodes newInstance() {
         return new OtherUsersCodes();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_other_users_codes, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i("OtherUsersCodes", "Looking at OtherUser's codes");
+        super.onCreate(savedInstanceState);
+//        binding = FragmentOtherUsersCodesBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+        // hide bottom navigation bar when viewing OtherUser's codes
+        MainActivity mainAct = (MainActivity) getActivity();
+        assert mainAct != null;
+        mainAct.bttmNavView.setVisibility(View.INVISIBLE);
+
+        mViewModel = new ViewModelProvider(this).get(OtherUsersCodesViewModel.class);
+        mViewModel.addTestData();  // add test data
+        Log.i("OtherUsersCodes", "codeDrawings: " + mViewModel.codeDrawings);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(OtherUsersCodesViewModel.class);
-        // TODO: Use the ViewModel
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_other_users_codes, container, false);
+
+        // create grid of OtherUser's codes
+        gridOfCodes = (GridView) view.findViewById(R.id.otherUsersCodes);
+        OtherUsersLibraryAdapter adapter = new OtherUsersLibraryAdapter(OtherUsersCodes.this.getContext(), mViewModel.codeNames, mViewModel.codeScores, mViewModel.codeDrawings);
+        gridOfCodes.setAdapter(adapter);
+
+        // go back to the OtherUser Profile
+        backBttn = (ImageButton) view.findViewById(R.id.backBttn);
+        backBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity mainAct = (MainActivity) getActivity();
+                assert mainAct != null;
+                mainAct.bttmNavView.setVisibility(View.VISIBLE);
+                mainAct.onBackPressed();
+            }
+        });
+
+        return view;
     }
+
 
 }
--- app/src/main/java/com/example/qrky/OtherUsersCodesViewModel.java
+++ app/src/main/java/com/example/qrky/OtherUsersCodesViewModel.java
@@ -2,7 +2,56 @@ package com.example.qrky;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+// US 02.03.01: As a player, I want to be able to browse QR codes that other players have scanned.
+// Artist: Franco Bonilla
+// Actors: Player (primary)
+// Goal: Player sees the code library of other players
+// Trigger: Player clicks on an OtherUser
+// Precondition: N/A
+// Postcondition: Player sees OtherUser's code library
+// Basic Flow:
+//     1. Player clicks on an OtherUser (trigger)
+//     2. System gets code library from database
+//     3. System makes card for each code in library
+//     4. System displays cards in a grid (3 columns)
+//     5. Player sees OtherUser's code library (goal)
 public class OtherUsersCodesViewModel extends ViewModel {
-    // US 02.03.01: As a player, I want to be able to browse QR codes that other players have scanned.
-    // Franco Bonilla
+    List<String> codeNames = new ArrayList<>();
+    List<Integer> codeScores = new ArrayList<>();
+    List<List<String>> codeDrawings = new ArrayList<>();  // 0 = eyes, 1 = nose, 2 = mouth
+
+    // TODO: get code library from database
+
+    // TODO: DELETE TEST DATA
+    public void addTestData() {
+        codeNames.add("CookieMonster");
+        codeScores.add(100);
+        List<String> codeDrawing = new ArrayList<String>();
+        codeDrawing.add("U     U");
+        codeDrawing.add("   ~   ");
+        codeDrawing.add("  ___  ");
+        codeDrawings.add(codeDrawing);
+
+
+        codeNames.add("Elmo");
+        codeScores.add(340);
+        List<String> codeDrawing2 = new ArrayList<String>();
+        codeDrawing2.add(">     <");
+        codeDrawing2.add("   -   ");
+        codeDrawing2.add("  UUU  ");
+        codeDrawings.add(codeDrawing2);
+
+        codeNames.add("DiegoZombie");
+        codeScores.add(8);
+        List<String> codeDrawing3 = new ArrayList<String>();
+        codeDrawing3.add("T     T");
+        codeDrawing3.add("   O   ");
+        codeDrawing3.add("  MWM  ");
+        codeDrawings.add(codeDrawing3);
+    }
+
 }
