9958ee04cf85ee0813972949406d051b5c7289c5
Jing <56137537+Maia580@users.noreply.github.com>
2023-03-12 03:47:04 -0600

    profile pop up added

--- app/src/main/java/com/example/qrky/MainActivity.java
+++ app/src/main/java/com/example/qrky/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.qrky;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
--- app/src/main/java/com/example/qrky/userProfileFragment.java
+++ app/src/main/java/com/example/qrky/userProfileFragment.java
@@ -1,64 +1,69 @@
 package com.example.qrky;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link userProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Represents an instance of user profile
  */
-public class userProfileFragment extends Fragment {
+public class userProfileFragment extends DialogFragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String Username;
+    private String Email;
+    private int Rank;
+    private int cardsCollected;
+    private int totalPoints;
 
     public userProfileFragment() {
-        // Required empty public constructor
+        // empty constructor
     }
-
+    @SuppressWarnings("unused")
+    public static userProfileFragment newInstance() {
+       userProfileFragment fragment = new userProfileFragment();
+       return fragment;
+    }
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment userProfileFragment.
+     * java docs
      */
-    // TODO: Rename and change types and number of parameters
-    public static userProfileFragment newInstance(String param1, String param2) {
-        userProfileFragment fragment = new userProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//
+//    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_user_profile, null);
+        AlertDialog.Builder  builder = new AlertDialog.Builder((getContext()));
+
+        // functions add here
+        // edit profile button
+        return builder
+                .setView(view)
+                .setNegativeButton("Exit", null)
+                .setPositiveButton("Ok", null).create();
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
-    }
 }
