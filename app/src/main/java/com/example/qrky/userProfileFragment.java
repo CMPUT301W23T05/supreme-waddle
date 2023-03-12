4723163d33d6455b8e09389bd847c497000b4dfc
Jing <56137537+Maia580@users.noreply.github.com>
2023-03-12 05:02:57 -0600

    Some Extra profile functionality and clean-up

--- app/src/main/java/com/example/qrky/userProfileFragment.java
+++ app/src/main/java/com/example/qrky/userProfileFragment.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -14,6 +13,8 @@ import android.view.View;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 import androidx.annotation.Nullable;
@@ -25,8 +26,9 @@ import androidx.fragment.app.DialogFragment;
 public class userProfileFragment extends DialogFragment {
 
 
-    private String Username;
+    private String Username = "@toBFrank";
     private String Email;
+    private String Password;
     private int Rank;
     private int cardsCollected;
     private int totalPoints;
@@ -43,21 +45,28 @@ public class userProfileFragment extends DialogFragment {
      * java docs
      */
 
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//
-//    }
-
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_user_profile, null);
         AlertDialog.Builder  builder = new AlertDialog.Builder((getContext()));
 
+
         // functions add here
-        // edit profile button
+        TextView username = view.findViewById(R.id.username);
+        TextView rank = view.findViewById(R.id.rank);
+        TextView cards_collected = view.findViewById(R.id.cards_collected);
+        TextView total_points = view.findViewById(R.id.total_points);
+
+        username.setText(getResources().getString(R.string.username1,Username));
+
+        Button edit_button = view.findViewById(R.id.edit_button);
+        edit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // edit profile button functionality (add later)
+            }
+        });
         return builder
                 .setView(view)
                 .setNegativeButton("Exit", null)
@@ -66,4 +75,28 @@ public class userProfileFragment extends DialogFragment {
     }
 
 
+
+    public int getRank() {
+        return Rank;
+    }
+
+    public void setRank(int rank) {
+        Rank = rank;
+    }
+
+    public int getCardsCollected() {
+        return cardsCollected;
+    }
+
+    public void setCardsCollected(int cardsCollected) {
+        this.cardsCollected = cardsCollected;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
 }
