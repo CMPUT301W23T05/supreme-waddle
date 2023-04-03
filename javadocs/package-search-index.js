7e8b6a051f193cb4ddf1c28844de1cca3c4bdc36
AARON <abinoy@ualberta.ca>
2023-04-03 15:36:38 -0600

    javadocs

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.example.qrky"},{"l":"com.example.qrky.data"},{"l":"com.example.qrky.data.model"},{"l":"com.example.qrky.placeholder"}]
